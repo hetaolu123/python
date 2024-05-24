7a84a3dcbe66647305823cf9a7ca47d94d93fcf5
hetaolu123 <167686497+hetaolu123@users.noreply.github.com>
2024-05-24 09:15:45 +0800

    Add files via upload

---
+++ text2.py
@@ -0,0 +1,34 @@
+def test_func(compute):
+    result = compute(3,4)
+    print(result)
+
+
+def compute(x,y):
+    return x + y
+
+
+test_func(compute)
+
+
+def test_func(compute):
+    result = compute(3, 4)
+    print(result)
+
+
+def compute(x, y):
+    return x * y
+
+
+test_func(compute)
+
+
+def test_func(compute):
+    result = compute(3, 4)
+    print(result)
+
+
+def compute(x, y):
+    return x - y
+
+
+test_func(compute)
